fb5e1f072323715133cfdcbca277218e1b12673c
Mike👨‍💻 <binarybardcode@gmail.com>
2024-09-16 20:16:11 +0200

    Implement Hero section

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+const Button = ({ name, isBeam = false, containerClass }) => {
+  return (
+    <button className={`btn ${containerClass}`}>
+        {isBeam && (
+            <span className="relative flex h-3 w-3">
+                <span className="btn-ping" />
+                <span className="btn-ping_dot" /> 
+            </span>
+        )}
+        {name}
+    </button>
+  )
+}
+
+export default Button 
---
+++ src/components/HeroCamera.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useRef } from "react"
+
+const HeroCamera = ({children }) => {
+    const groupRef = useRef(); 
+
+    useFrame(( state, delta) => {
+        easing.damp3(state.camera.position, [0, 0, 20], 0.25, delta)
+
+    //    if (!isMobile) {
+            easing.dampE(groupRef.current.rotation, [-state.pointer.y / 3, -state.pointer.x / 5, 0], 0.25, delta)
+    //    }
+    })
+    
+  return (
+    <group ref={groupRef} scale={1.0}>
+        {children}
+    </group>
+  )
+}
+
+export default HeroCamera
